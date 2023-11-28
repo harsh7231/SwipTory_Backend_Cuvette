2040f07ff7397db01712054ab790c9c3524a05a2
Harsh Kumar rattewal <harshrattewal@gmail.com>
2023-11-28 11:06:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// Import required packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const mongoose = require("mongoose");
+
+// Import routes
+const userRoutes = require("./routes/userRoutes.js");
+const storyRoutes = require("./routes/storyRoutes.js");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const corsOptions = {
+  credentials: true,
+  origin: "https://swip-tory-liard.vercel.app/",
+};
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes
+app.use("/api/user", userRoutes);
+app.use("/api/story", storyRoutes);
+
+//DB Server
+
+app.listen(process.env.PORT, () => {
+  mongoose
+    .connect(process.env.DB_CONNECT, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB Connected");
+      console.log(`App listening at http://localhost:${process.env.PORT}`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
